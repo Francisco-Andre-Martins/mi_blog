1a1130756b89fe31dae06c691a46cf5b3ffddeca
Francisco Martins <francisco.andre.carvalho.martins@tecnico.ulisboa.pt>
2025-09-15 22:43:20 +0100

    first commit

---
+++ blog/src/main.rs
@@ -0,0 +1,11 @@
+//function: read from file (reads from given path, returns string)
+fn read_from_file(){
+    
+}
+//function: parse into paragraphs
+//function: convert into HTML
+//function: write into file
+//main: reads files from directory (supplied in .env? assume . at first, NON RECURSIVE FOR STARTERS)
+fn main() {
+    println!("Hello, world!");
+}
