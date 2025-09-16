3198f2e6485167370ee2c121000004f33ca49f2f
Francisco Martins <francisco.andre.carvalho.martins@tecnico.ulisboa.pt>
2025-09-16 15:57:47 +0100

    flakes the begginign

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "A very basic flake to import all project dependencies";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: 
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          default = pkgs.mkShellNoCC {
+            packages = [
+              pkgs.rustfmt
+              pkgs.cargo
+            ];
+          };
+        }
+      );
+
+    };
+}
