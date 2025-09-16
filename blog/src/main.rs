11f27439daeb2e2647eb1eff406d95e0f9fabfb5
Francisco Martins <francisco.andre.carvalho.martins@tecnico.ulisboa.pt>
2025-09-16 23:14:41 +0100

    version 0.0.0.1 is complete i suppose

--- blog/src/main.rs
+++ blog/src/main.rs
@@ -1,4 +1,23 @@
 use std::{fs,env};
+fn directory_creator()->String{
+    String::from("<!DOCTYPE html> <html lang=\"pt\"> <head><meta charset=\"UTF-8\"><link rel=\"stylesheet\" href=\"style.css\"><title>Stuff </title></head><body><div class=\"content\"><h1>Posts</h1>")
+
+}
+fn add_link_to_directory(path:String, pagetitle:&str)->String{
+    let mut return_string=String::new();
+    return_string.push_str("<a href=");
+    println!("{path}");
+    return_string.push_str(&path);
+    return_string.push_str(".html");
+
+    return_string.push_str(">");
+    return_string.push_str(&pagetitle);
+    return_string.push_str("<a>");
+    return return_string;
+}
+fn end_directory()->String{
+    String::from("</div></body></html>")
+}
 //function: read from file (reads from given path, returns string)
 fn read_from_file(path_to_read:String)->String{
     
@@ -54,22 +73,35 @@ fn main() {
     let paths = fs::read_dir(args[1].clone()).unwrap();
 
     let outputdir =&args[2];
+    let mut directory_page=directory_creator();
+
+    //This part of the code will assemble the content pages
     for path in paths {
             let mut new_path=path.unwrap();
             if !new_path.path().is_dir(){
-                let mut name=  new_path.path().into_os_string().into_string().unwrap();
+                let name=  new_path.path().into_os_string().into_string().unwrap();
+
                 let stuff=read_from_file(name.clone());
                 let paragraphs: Vec<&str>= stuff.split('\n').collect();
-                let validhtml: String= convert_into_post(paragraphs);
+
+ 
                 let mut new_name=String::from(outputdir);
                 new_name.push('/');
                 new_name.push_str(&new_path.file_name().into_string().unwrap());
                 new_name.push_str(".html");
+                directory_page.push_str(&add_link_to_directory(String::from(&new_path.file_name().into_string().unwrap()),paragraphs[0]));
+                let validhtml: String= convert_into_post(paragraphs);
                 write_into_file(new_name, validhtml); 
             }
-
-
-
     }
+    directory_page.push_str(&end_directory()); 
+    let mut directory_path = String::from(outputdir);
+    println!("I am yelling this {directory_path}");
+    directory_path.push_str("/directory.html");
+    write_into_file(directory_path, directory_page);
+    //This part of the code assembles the directory
+    //I first want to see what the default sort order is
+    //sorted_paths.sort();
+
     println!("this didn't blow up (surprisingly)");
 }
